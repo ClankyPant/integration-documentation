0f39bf3c2d5981067e8fb34ec5221cf568669042
Luis Albino <luis@ibtech.inf.br>
2022-03-04 21:12:58 -0300

    WIP

--- src/components/EditorVue.vue
+++ src/components/EditorVue.vue
@@ -1,10 +1,10 @@
 <template>
   <v-card width="100%" height="100%">
     <v-card-title v-show="fileSelected == false"> Select a file </v-card-title>
-
     <div v-show="fileSelected">
       <v-card-title> Document title </v-card-title>
       <v-card-text>
+        <EditorMenu v-model="editor" />
         <editor-content :editor="editor" />
       </v-card-text>
     </div>
@@ -15,17 +15,22 @@
 import { Component, Vue } from "vue-property-decorator";
 import { Editor, EditorContent } from "@tiptap/vue-2";
 import StarterKit from "@tiptap/starter-kit";
-import Document from "@tiptap/extension-document";
-import Paragraph from "@tiptap/extension-paragraph";
-import Text from "@tiptap/extension-text";
-import Heading from "@tiptap/extension-heading";
+import EditorMenu from "@/components/EditorMenu.vue";
 
-@Component({ components: { EditorContent } })
+@Component({ components: { EditorContent, EditorMenu } })
 export default class EditorVue extends Vue {
   fileSelected = true;
+
   editor = new Editor({
     content: "<p>I’m running Tiptap with Vue.js. 🎉</p>",
-    extensions: [StarterKit, Document, Paragraph, Text, Heading],
+    extensions: [StarterKit],
+    editorProps: {
+      attributes: {
+        class: "teste",
+      },
+    },
   });
 }
+
+console.log(EditorMenu);
 </script>

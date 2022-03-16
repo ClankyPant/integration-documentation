784100e746c7a204c7b458a8c31bec161ced3025
Luis Albino <luis@ibtech.inf.br>
2022-03-16 20:57:43 -0300

    WIP

--- src/components/EditorVue.vue
+++ src/components/EditorVue.vue
@@ -3,10 +3,17 @@
     <v-card-title v-show="fileSelected == false"> Select a file </v-card-title>
     <div v-show="fileSelected">
       <v-card-title> Document title </v-card-title>
-      <v-card-text>
-        <div id="editorJs"></div>
-        <v-btn @click="save()">Salvar</v-btn>
+      <v-card-text class="outside-editor-content">
+        <v-card>
+          <v-card-text>
+            <div id="editorJs"></div>
+          </v-card-text>
+        </v-card>
       </v-card-text>
+      <v-card-actions>
+        <v-btn dark @click="save()">Salvar</v-btn>
+        <v-btn dark @click="save()">Desfazer</v-btn>
+      </v-card-actions>
     </div>
   </v-card>
 </template>
@@ -22,9 +29,16 @@ export default class EditorVue extends Vue {
   editor = EditorService.getEditor();
 
   async save(): Promise<void> {
-     this.editor.save().then((result) => {
+    this.editor.save().then((result) => {
       console.log(result);
     });
   }
 }
 </script>
+
+<style scoped>
+.outside-editor-content {
+  padding: 0 130px 0 130px;
+  min-height: 80vh;
+}
+</style>
--- src/services/EditorService.ts
+++ src/services/EditorService.ts
@@ -1,6 +1,5 @@
 import EditorJS from "@editorjs/editorjs";
 const EditorTable = require("@editorjs/table");
-const editorParser = require("editorjs-html");
 
 export default class EditorService {
   public static getEditor(): EditorJS {
